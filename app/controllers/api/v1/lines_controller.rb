2222f7b5aa7575e0ff0d8dcd2821e43206eca65f
Bruno Montebello <brunormontebello@hotmail.com>
2025-02-28 17:00:53 -0300

    Refactor caching for better readability

--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -3,25 +3,19 @@ module Api
     class LinesController < ApplicationController
       def index
 
-        # cached_keys = Rails.cache.fetch("cached_line_keys") { [] }
+        cached_keys = Rails.cache.fetch("cached_line_keys") { [] }
 
-        # @cached_lines = cached_keys.each_with_object({}) do |key, hash|
-        #   hash[key] = Rails.cache.read(key)
-        # end
+        @cached_lines = cached_keys.each_with_object({}) do |key, hash|
+          hash[key] = Rails.cache.read(key)
+        end
       end
 
       def show
         line_index = params[:id].to_i
 
-        unless TextFile.valid_line_index?(line_index)
-          Rails.logger.warn { '*** Line index out of range' }
+        validate_line_index(line_index)
 
-          render json: { error: "*** Line index out of range" }, status: :unprocessable_entity
-          return
-        end
-
-        cache_key    = "line_#{line_index}"
-        line_content = fetch_or_cache_line(cache_key, line_index)
+        line_content = fetch_or_cache_line(line_index)
 
         if line_content.present?
           Rails.logger.info("*** Line content found -> #{line_content}")
@@ -38,7 +32,17 @@ module Api
 
       private
 
-      def fetch_or_cache_line(cache_key, line_index)
+      def validate_line_index(line_index)
+        return if TextFile.valid_line_index?(line_index)
+
+        Rails.logger.warn { '*** Line index out of range' }
+
+        render json: { error: "Line index out of range" }, status: :unprocessable_entity
+      end
+
+      def fetch_or_cache_line(line_index)
+        cache_key = "line_#{line_index}"
+
         Rails.cache.fetch(cache_key, expires_in: 1.day) do
           content = LineReaderService.fetch_line(line_index)
           track_cache_key(cache_key) if content.present?
