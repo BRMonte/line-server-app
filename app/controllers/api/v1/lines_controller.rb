aedc98ca612d42b07ac7fcf5311928352026c0c5
Bruno Montebello <brunormontebello@hotmail.com>
2025-02-27 21:49:14 -0300

    Create lines controller and index, show and search actions

---
+++ app/controllers/api/v1/lines_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module v1
+    class LinesController < ApplicationController
+      def index
+      end
+
+      def show
+      end
+
+      def search
+      end
+    end
+  end
+end
