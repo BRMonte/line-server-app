e5bbfa21c020d674fc9b5f319493d543e18d0f58
Bruno Montebello <brunormontebello@hotmail.com>
2025-02-28 09:45:52 -0300

    Delete search action + implement show action

--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -1,13 +1,24 @@
 module Api
-  module v1
+  module V1
     class LinesController < ApplicationController
       def index
       end
 
       def show
-      end
+        line_index = params[:line_index].to_i
 
-      def search
+        if line_index.negative? || line_index >= TextFile.total_lines
+          render json: { error: "Line index out of range" }, status: :unprocessable_entity
+          return
+        end
+
+        line_content = LineReaderService.fetch_line(line_index)
+
+        if line_content.present?
+          render json: { line_index: line_index, content: line_content }, status: :ok
+        else
+          render json: { error: "Line not found" }, status: :not_found
+        end
       end
     end
   end
