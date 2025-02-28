8b80b7d986e02fa75e9c39f450018883f5b56225
Bruno Montebello <brunormontebello@hotmail.com>
2025-02-28 09:44:24 -0300

    Remove search route + set index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  root "api/v1/lines#search"
+  root "api/v1/lines#index"
 
   namespace :api do
     namespace :v1 do
-      resources :lines, only: [:index, :show] do
-        collection do
-          get "search"
-        end
-      end
+      resources :lines, only: [:index, :show]
     end
   end
 end
