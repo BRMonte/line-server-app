15d777eb2dd9940afcfd74c8b45d31f5d34c5ea2
Bruno Montebello <brunormontebello@hotmail.com>
2025-02-28 11:14:42 -0300

    Set up caching for requested lines

--- app/services/line_reader_service.rb
+++ app/services/line_reader_service.rb
@@ -1,18 +1,21 @@
 class LineReaderService
   FILE_PATH = Rails.root.join("storage", "test.txt")
+  CACHE_EXPIRY = 1.day
 
   def self.fetch_line(line_index)
-    Rails.logger.debug('*** Method fetch_line entered')
-    return nil unless File.exist?(FILE_PATH)
+    Rails.cache.fetch("line_#{line_index}", expires_in: CACHE_EXPIRY) do
+      Rails.logger.debug('*** Method fetch_line entered')
+      return nil unless File.exist?(FILE_PATH)
 
-    File.foreach(FILE_PATH).with_index do |line, index|
-      if index == line_index
-        Rails.logger.debug('*** Line found')
-        return line.strip
+      File.foreach(FILE_PATH).with_index do |line, index|
+        if index == line_index
+          Rails.logger.debug('*** Line found')
+          return line.strip
+        end
       end
-    end
 
-    Rails.logger.debug('*** Line not found')
-    nil
+      Rails.logger.debug('*** Line not found')
+      nil
+    end
   end
 end
