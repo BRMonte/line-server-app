0ac55b11e93fc59263514a3273385502bd8facde
Bruno Montebello <brunormontebello@hotmail.com>
2025-02-28 19:35:28 -0300

    Refactor service variables for better readability

--- app/services/line_reader_service.rb
+++ app/services/line_reader_service.rb
@@ -1,15 +1,21 @@
 class LineReaderService
+  SIZE_LIMIT_FOR = {
+    sequential_processing: 3.gigabytes,
+    parallel_processing: 10.gigabytes,
+  }
   SEQUENTIAL_PROCESSING_FILE_SIZE = 3.gigabytes
   PARALLEL_PROCESSING_FILE_SIZE   = 10.gigabytes
 
+  @text_file = TextFile.instance
+
   def self.fetch_line(line_index)
-    raise "File not found" unless TextFile.exists?
+    raise "File not found" unless @text_file.exists?
 
-    file_size = TextFile.file_size
+    file_size = @text_file.file_size
 
-    if file_size <= SEQUENTIAL_PROCESSING_FILE_SIZE
+    if file_size <= SIZE_LIMIT_FOR[:sequential_processing]
       process_sequentially(line_index)
-    elsif file_size <= PARALLEL_PROCESSING_FILE_SIZE
+    elsif file_size <= SIZE_LIMIT_FOR[:parallel_processing]
       process_in_parallel(line_index)
     end
   end
@@ -19,7 +25,7 @@ class LineReaderService
   def self.process_sequentially(line_index)
     Rails.logger.debug('*** Processing sequentially')
 
-    File.foreach(TextFile::FILE_PATH).with_index do |line, index|
+    File.foreach(@text_file.class::FILE_PATH).with_index do |line, index|
       return line.strip if index == line_index
     end
 
@@ -31,7 +37,7 @@ class LineReaderService
     Rails.logger.debug('*** Processing in parallel')
 
     start_index = 0
-    chunk_size  = (TextFile.total_lines / 4.0).ceil
+    chunk_size  = (@text_file.total_lines / 4.0).ceil
     jobs        = []
 
     4.times do |i|
@@ -39,7 +45,7 @@ class LineReaderService
         start_line = start_index + (i * chunk_size)
         end_line = start_index + ((i + 1) * chunk_size) - 1
 
-        File.foreach(TextFile::FILE_PATH).with_index do |line, index|
+        File.foreach(@text_file.class::FILE_PATH).with_index do |line, index|
           next unless index.between?(start_line, end_line)
           return line.strip if index == line_index
         end
