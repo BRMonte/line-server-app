891d3af6a2a9de6f366e62fcb6bdc1c6233f586d
Bruno Montebello <brunormontebello@hotmail.com>
2025-02-28 11:01:26 -0300

    Place loggers

--- app/services/line_reader_service.rb
+++ app/services/line_reader_service.rb
@@ -2,12 +2,17 @@ class LineReaderService
   FILE_PATH = Rails.root.join("storage", "test.txt")
 
   def self.fetch_line(line_index)
+    Rails.logger.debug('*** Method fetch_line entered')
     return nil unless File.exist?(FILE_PATH)
 
     File.foreach(FILE_PATH).with_index do |line, index|
-      return line.strip if index == line_index
+      if index == line_index
+        Rails.logger.debug('*** Line found')
+        return line.strip
+      end
     end
 
+    Rails.logger.debug('*** Line not found')
     nil
   end
 end
