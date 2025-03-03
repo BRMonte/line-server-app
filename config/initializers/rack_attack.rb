682da8c80388028de1173be351cac93d4f5aac5f
Bruno Montebello <brunormontebello@hotmail.com>
2025-03-03 10:26:00 -0300

    Be more spepecific on rack-attack target endpoint

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,6 +4,13 @@ class Rack::Attack
     req.ip
   end
 
+  # Throttle specific endpoint (10 requests per minute for the lines#show endpoint)
+  throttle('lines/show', limit: 10, period: 1.minute) do |req|
+    if req.path == '/api/v1/lines' && req.params['id'].present?
+      req.ip
+    end
+  end
+
   # Custom response when throttled
   self.throttled_response = lambda do |env|
     [429, {}, [{ error: "Too many requests. Please try again later." }.to_json]]
