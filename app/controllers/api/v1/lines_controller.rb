027a3beb46f2d06bc59085f5cd8812fb9ee8dd8e
Bruno Montebello <brunormontebello@hotmail.com>
2025-03-03 11:39:03 -0300

    Remove *** from logs

--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -13,14 +13,14 @@ module Api
         line_content = LineCaching.fetch_or_cache(line_index, line_reader_service: LineReaderService)
 
         if line_content.present?
-          Rails.logger.info("*** Line content found -> #{line_content}")
+          Rails.logger.info("Line content found -> #{line_content}")
           render json: { line_index: line_index, content: line_content }, status: :ok
         else
-          Rails.logger.warn { "*** Request returned #{response.code}, #{response.body}" }
+          Rails.logger.warn { "Request returned #{response.code}, #{response.body}" }
           render json: { error: "Line not found" }, status: :not_found
         end
       rescue => e
-        logger.error { "*** #{e}: #{e.message}" }
+        logger.error { "#{e}: #{e.message}" }
         render json: { error: "Internal Server Error" }, status: :internal_server_error
       end
 
@@ -32,7 +32,7 @@ module Api
 
       def validate_line_index(line_index)
         unless TextFile.instance.valid_line_index?(line_index)
-          Rails.logger.warn('*** Line index out of range')
+          Rails.logger.warn('Line index out of range')
           render json: { error: "Line index out of range" }, status: :unprocessable_entity
           return true
         end
